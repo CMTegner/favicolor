07c19dcbbae52e39f1e59fe471268817b0bffa6e
Christian Tegnér <christian.tegner@gmail.com>
2014-08-21 21:56:37 +0200

    Long lines begone!

--- test/test.js
+++ test/test.js
@@ -15,20 +15,26 @@ test('should return true on success', function(t) {
 test('should return accept a string', function(t) {
     var icon = ic();
     t.equals(favicon(icon, 'red'), true);
-    t.equals(icon.href, 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEwAACxMBAJqcGAAAABBJREFUCB0BBQD6/wD/AAD/BQAB/yStDWMAAAAASUVORK5CYII=');
+    t.equals(icon.href, 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAAB' +
+        'CAYAAAAfFcSJAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEwAACxMBAJqcGAAAABBJRE' +
+        'FUCB0BBQD6/wD/AAD/BQAB/yStDWMAAAAASUVORK5CYII=');
     t.end();
 });
 
 test('should return accept an array', function(t) {
     var icon = ic();
     t.equals(favicon(icon, [0, 255, 0]), true);
-    t.equals(icon.href, 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEwAACxMBAJqcGAAAABBJREFUCB0BBQD6/wAA/wD/BAEB/ymuTOcAAAAASUVORK5CYII=');
+    t.equals(icon.href, 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAAB' +
+        'CAYAAAAfFcSJAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEwAACxMBAJqcGAAAABBJRE' +
+        'FUCB0BBQD6/wAA/wD/BAEB/ymuTOcAAAAASUVORK5CYII=');
     t.end();
 });
 
 test('should return accept separate RGB numbers', function(t) {
     var icon = ic();
     t.equals(favicon(icon, 0, 0, 255), true);
-    t.equals(icon.href, 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEwAACxMBAJqcGAAAABBJREFUCB0BBQD6/wAAAP//AwIB/8DNK/sAAAAASUVORK5CYII=');
+    t.equals(icon.href, 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAAB' +
+        'CAYAAAAfFcSJAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEwAACxMBAJqcGAAAABBJRE' +
+        'FUCB0BBQD6/wAAAP//AwIB/8DNK/sAAAAASUVORK5CYII=');
     t.end();
 });
